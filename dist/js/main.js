f30b394e7251d0b23c89d4dcb98a80e3c5f83cf6
KaroliinaAaltonen <aaltonen.laura00@gmail.com>
2024-02-11 01:16:21 +0200

    welcome to 2024

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,59 @@
-//Select DOM Items
-
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuBranding= document.querySelector('.menu-branding');
+const menuBranding = document.querySelector('.menu-branding');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.nav-item');
+const detailedHeader = document.querySelector('.job-detailed-header');
+const detailedListElements = document.querySelectorAll('.detailed-list-child-element');
 
-//Set Initial State of Menu
-let showMenu = false; //'let' can directly reassing on certain time and const cannot do that
+// Set Initial State of Menu
+let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-    if(!showMenu){
+// Toggle menu function
+function toggleMenu() {
+    if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        //Reset Menu State
+        // Reset Menu State
         showMenu = true;
-    }else{
+    } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
-        //Reset Menu State
+        // Reset Menu State
         showMenu = false;
-
     }
 }
+
+// Toggle detailed list function
+function toggleDetailedList() {
+    const detailedList = document.querySelector('.detailed-list');
+    detailedList.classList.toggle('show'); // Toggle the 'show' class
+}
+
+// Toggle detailed list element function
+function toggleDetailedListElement(event) {
+    // Check if the clicked element is the detailed list child element
+    if (event.target.classList.contains('detailed-list-child-element')) {
+        // Toggle the 'show' class of the detailed list content
+        const detailedListContent = event.target.querySelector('.detailed-list-child-content');
+        detailedListContent.classList.toggle('show');
+    }
+}
+
+// Event listeners
+menuBtn.addEventListener('click', toggleMenu);
+detailedHeader.addEventListener('click', toggleDetailedList);
+
+// Assign click event listener to each detailed list item
+detailedListElements.forEach(item => {
+    item.addEventListener('click', toggleDetailedListElement);
+});
